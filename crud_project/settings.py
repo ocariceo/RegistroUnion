ceda2ed39d5a0d8ee16ee81aefb3ec176e6c6578
ocariceo <ocariceor@yahoo.es>
2021-08-19 11:24:11 -0400

    Update settings.py

--- crud_project/settings.py
+++ crud_project/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vl3#09%%lqj$2pg3m@g*lqhqng(lr3@i@&hjxvl4zwmxgzghpx'
+# SECURITY WARNING: keep the secret key used in production 
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxx'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
